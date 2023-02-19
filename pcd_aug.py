c66059c95c75ed75ed6f621c309a0bd3344262b7
Koray Aksu <101435018+aksuko@users.noreply.github.com>
2023-02-19 23:58:28 +0300

    Update pcd_aug.py

--- pcd_aug.py
+++ pcd_aug.py
@@ -1,17 +1,22 @@
 import os
 import numpy as np
+import argparse
 
-# Define the probability of performing each augmentation
-probability = {
-    "jittering": 0.6,
-    "scaling": 0.6,
-    "shifting": 0.6,
-    "rotating": 0,
-    "rgb_jittering": 0.25,
-    "rgb_light_effect": 0.25
-}
+parser = argparse.ArgumentParser(description='Perform data augmentation on a given dataset.')
+parser.add_argument('--input_folder', type=str, help='the path to the input folder')
+parser.add_argument('--output_folder', type=str, help='the path to the output folder')
+parser.add_argument('--jittering', type=float, default=0.6, help='the probability of performing jittering (default: 0.6)')
+parser.add_argument('--scaling', type=float, default=0.6, help='the probability of performing scaling (default: 0.6)')
+parser.add_argument('--shifting', type=float, default=0.6, help='the probability of performing shifting (default: 0.6)')
+parser.add_argument('--rotating', type=float, default=0, help='the probability of performing rotating (default: 0)')
+parser.add_argument('--rgb_jittering', type=float, default=0.25, help='the probability of performing RGB jittering (default: 0.25)')
+parser.add_argument('--rgb_light_effect', type=float, default=0.25, help='the probability of performing RGB light effect (default: 0.25)')
+parser.add_argument('--aug_num', type=int, default=50, help='number of the augmentation of each class')
+args = parser.parse_args()
 
-# Define the augmentation functions
+
+
+# The augmentation functions
 def jittering(data, prob):
     if np.random.uniform() < prob:
         data[:,:3] += np.random.normal(0, 0.01, size=data[:,:3].shape)
@@ -51,53 +56,43 @@ def rgb_light_effect(data, prob):
         return "rgb_light_effect"
     return ""
 
-# Define the input and output folder
-input_folder = r"path_of_input_folder"
-output_folder = r"path_of_output_folder"
-
-# Create processed directory if not exists
-os.makedirs(output_folder, exist_ok=True)
-
-# Create an augmentation report file
-report_file = open(os.path.join(output_folder, "augmentation_report.txt"), "w")
+os.makedirs(args.output_folder, exist_ok=True)
+report_file = open(os.path.join(args.output_folder, "augmentation_report.txt"), "w")
 
 # Process all subfolders in the input folder (class labels)
-for class_folder in os.listdir(input_folder):
-    class_folder_path = os.path.join(input_folder, class_folder)
+for class_folder in os.listdir(args.input_folder):
+    class_folder_path = os.path.join(args.input_folder, class_folder)
     if os.path.isdir(class_folder_path):
         report_file.write("Class: {}\n".format(class_folder))
         # Create a corresponding subfolder in the output folder
-        output_class_folder_path = os.path.join(output_folder, class_folder)
+        output_class_folder_path = os.path.join(args.output_folder, class_folder)
         if not os.path.exists(output_class_folder_path):
             os.makedirs(output_class_folder_path)
         
-        # Generate random indices of the examples 
-        examples = os.listdir(class_folder_path)
-        examples_count = len(examples)
-        sample_count = 0
-        while sample_count < 20:
-            random_index = np.random.randint(0, examples_count)
-            example_path = os.path.join(class_folder_path, examples[random_index])
-            if example_path.endswith('.txt'):
-                # Load the data into a numpy array
-                data = np.genfromtxt(example_path, delimiter=' ')
-                applied_augmentations = []
-                # Apply the augmentation functions
-                applied_augmentations.append(jittering(data, probability["jittering"]))
-                applied_augmentations.append(scaling(data, probability["scaling"]))
-                applied_augmentations.append(shifting(data, probability["shifting"]))
-                applied_augmentations.append(rotating(data, probability["rotating"]))
-                applied_augmentations.append(rgb_jittering(data, probability["rgb_jittering"]))
-                applied_augmentations.append(rgb_light_effect(data, probability["rgb_light_effect"]))
-                applied_augmentations = list(filter(None, applied_augmentations))
-                # Save the augmented data to the output folder
-                output_file_path = os.path.join(output_class_folder_path, f"{data}_{sample_count}.txt")
-                np.savetxt(output_file_path, data, delimiter=' ', fmt='%.3f %.3f %.3f %d %d %d')
-                # Print the name of processed file and applied augmentations
-                print("Processed: ", example_path, "with augmentations: ", applied_augmentations)
-                # Write the augmentation report
-                report_file.write(f"{example_path}, {applied_augmentations}\n")
-                sample_count += 1
 
-# Close the augmentation report file
+        pcd = os.listdir(class_folder_path)
+        pcd_count = len(pcd)
+        sample_count = 0
+        while sample_count < args.aug_num:
+            random_index = np.random.randint(0, pcd_count)
+            pcd_path = os.path.join(class_folder_path, pcd[random_index])
+            if pcd_path.endswith('.txt'):
+                data = np.genfromtxt(pcd_path, delimiter=' ')
+                applied_augmentations = []
+                applied_augmentations.append(jittering(data, args.jittering))
+                applied_augmentations.append(scaling(data, args.scaling))
+                applied_augmentations.append(shifting(data, args.shifting))
+                applied_augmentations.append(rotating(data, args.rotating))
+                applied_augmentations.append(rgb_jittering(data, args.rgb_jittering))
+                applied_augmentations.append(rgb_light_effect(data, args.rgb_light_effect))
+                applied_augmentations = list(filter(None, applied_augmentations))
+
+                output_file_path = os.path.join(output_class_folder_path, f"{class_folder}_{sample_count}.txt")
+                np.savetxt(output_file_path, data, delimiter=' ', fmt='%.3f %.3f %.3f %.3f %.3f %.3f')
+
+                print("Processed: ", pcd_path, "with augmentations: ", applied_augmentations)
+                
+                # Write the augmentation report
+                report_file.write(f"{pcd_path}, {applied_augmentations}\n")
+                sample_count += 1
 report_file.close()
